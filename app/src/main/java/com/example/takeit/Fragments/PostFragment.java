b2485a19c1b69571be06f53f61d9110b45fb0023
synkling <bachurusu@gmail.com>
2021-03-30 15:52:48 -0400

    Camera working now.

--- app/src/main/java/com/example/takeit/Fragments/PostFragment.java
+++ app/src/main/java/com/example/takeit/Fragments/PostFragment.java
@@ -107,7 +107,7 @@ public class PostFragment extends Fragment {
         // wrap File object into a content provider
         // required for API >= 24
         // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.example.takeit.fileprovider", photoFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
 
         // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
